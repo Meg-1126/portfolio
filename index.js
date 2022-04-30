34e14bf140bd5086277621289acc381a5af614b2
Megumi Akama <kamayatu91@gmail.com>
2022-04-30 15:17:02 -0700

    Change padding of input tag and JS condition

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-window.addEventListener("scroll", function(){
-  const header = document.querySelector("header");
-  header.classList.toggle("scroll-nav", window.scrollY > 50);
-});
+if (window.matchMedia("(min-width: 577px)").matches) {
+  window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("scroll-nav", window.scrollY > 50);
+  });
+} 

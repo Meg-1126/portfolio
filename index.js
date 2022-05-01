06d0a5f639010dd0c56f2d4d1601bdabd8ae3be5
Megumi Akama <kamayatu91@gmail.com>
2022-05-01 16:57:21 -0700

    Add menu button for smartphone & description for projects

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+// Applicable only for large window
 if (window.matchMedia("(min-width: 577px)").matches) {
   window.addEventListener("scroll", function(){
     const header = document.querySelector("header");
     header.classList.toggle("scroll-nav", window.scrollY > 50);
   });
 } 
+
+function smallMenu () {
+  document.getElementById('line1').classList.toggle('line_1');
+  document.getElementById('line2').classList.toggle('line_2');
+  document.getElementById('line3').classList.toggle('line_3');
+  document.getElementById('nav-sp').classList.toggle('menu-in');
+}
+
+document.getElementById('hamburger').addEventListener('click', function(){
+  smallMenu();
+});

3d493a46628fcd996f35073706bd8a73f268bd24
Megumi Akama <kamayatu91@gmail.com>
2023-06-13 14:06:57 -0700

    Add new pages

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 // Applicable only for large window
 if (window.matchMedia("(min-width: 577px)").matches) {
-  window.addEventListener("scroll", function(){
+  window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     header.classList.toggle("scroll-nav", window.scrollY > 50);
   });
-} 
+}
 
-$(function(){
-$('.tab-btn').click(function(){
-  let index = $('.tab-btn').index(this);
-  $('.tab-btn').removeClass('active');
-  $(this).addClass('active');
-  $('.content').hide().eq(index).show();
-})
+$(function () {
+  $(".tab-btn").click(function () {
+    let index = $(".tab-btn").index(this);
+    $(".tab-btn").removeClass("active");
+    $(this).addClass("active");
+    $(".content").hide().eq(index).show();
+  });
 
-$('.menu-trigger').on('click', function() {
-  $(this).toggleClass('active');
-  $('#nav-sp').toggleClass('active');
-  return false;
+  $(".menu-trigger").on("click", function () {
+    $(this).toggleClass("active");
+    $("#nav-sp").toggleClass("active");
+    return false;
+  });
 });
-});
-
